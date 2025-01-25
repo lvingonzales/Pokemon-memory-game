fd932e7bb9e356bd4dc8851b95e8988d2c107c60
lvingonzales <lvingonzales15@gmail.com>
2025-01-25 01:28:44 -0500

    Added instructions section, description section and score section, Score section not functional.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import PlayArea from "./components/PlayArea";
 import "./App.css";
+import Score from "./components/Score";
 
 export default function App() {
   return (
     <>
-      <div className="game-screen"><PlayArea /></div>
+      <div className="game-screen">
+        <PlayArea />
+      </div>
       <div className="game-util">
-        <div className="game-info">Info Section</div>
-        <div className="game-stats">Stats Section</div>
+        <div className="game-info">
+          <h1>Pokemon Memory Game</h1>
+          <h2>by Liam Gonzales</h2>
+          <p>
+            This is a short memory based game I made using React. I used the
+            <a href="https://pokeapi.co/"> PokeApi </a> to get the images and
+            stored a pre-determined set to reduce the number of API calls.
+          </p>
+          <h2>Instructions:</h2>
+          <p>
+            The goal of the game is to click on as many pokemon as you can
+            <em> without clicking on any twice</em>. Each time you click on a
+            card the order of the cards will be shuffled.
+          </p>
+          <p>
+            Clicking on a pokemon more than once will <em> reset </em> your
+            score.
+          </p>
+        </div>
+        <div className="game-stats">
+          <h1>Current Score</h1>
+          <Score />
+        </div>
       </div>
     </>
   );
---
+++ src/components/Score.jsx
@@ -0,0 +1,5 @@
+import "../styles/Score.css";
+
+export default function Score () {
+    return <p className="score">0</p>
+}
